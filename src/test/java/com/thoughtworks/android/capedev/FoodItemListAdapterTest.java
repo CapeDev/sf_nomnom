8797b9566098754e6d4969ed3f20756ab02ae329
Errol Pais <epais@thoughtworks.com>
2013-03-09 21:30:35 -0800

    removed mock context, added Robolectric application

--- src/test/java/com/thoughtworks/android/capedev/FoodItemListAdapterTest.java
+++ src/test/java/com/thoughtworks/android/capedev/FoodItemListAdapterTest.java
@@ -1,31 +1,22 @@
 package com.thoughtworks.android.capedev;
 
-import android.content.Context;
 import com.xtremelabs.robolectric.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 
 import java.util.ArrayList;
 import static junit.framework.Assert.*;
-import static org.mockito.Mockito.*;
 
 @RunWith(RobolectricTestRunner.class)
 public class FoodItemListAdapterTest {
-
-    @Test
-    public void shouldBeTrue(){
-        assertTrue(true);
-    }
 
     @Test
     public void shouldReturnCount(){
         ArrayList<FoodItem> foods = new ArrayList<FoodItem>(10);
         foods.add(new FoodItem("baklava", 1));
         foods.add(new FoodItem("tiramisu", 2));
-        Context contextMock = mock(Context.class, Mockito.CALLS_REAL_METHODS); //for mocking abstract classes with mockito
 
-        FoodItemListAdapter adapter = new FoodItemListAdapter(contextMock, foods);
+        FoodItemListAdapter adapter = new FoodItemListAdapter(Robolectric.application.getApplicationContext(), foods);
 
         assertEquals(adapter.getCount(), 2);
     }
