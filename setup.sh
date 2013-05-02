58c5674b5cbfee8df4aaa76c17dab14fb3cd0898
Sheroy <smarker@thoughtworks.com>
2013-05-02 14:55:07 -0700

    setup script to install sdk locally with the project, with an emulator and other tools

--- setup.sh
+++ setup.sh
@@ -2,28 +2,27 @@
 
 set -e
 
-PROJECT_ROOT=$(/usr/bin/cd $(dirname $0) && pwd)
+ANDROID_SDK_ARTIFACT=android-sdk_r21.1-macosx.zip
 
-ANDROID_SDK_ARTIFACT=adt-bundle-mac-x86_64-20130219.zip
-
+PROJECT_ROOT=$(pwd)
+echo $PROJECT_ROOT
 VENDOR_DIRECTORY=$PROJECT_ROOT/vendor
 if [ ! -d $VENDOR_DIRECTORY ]; then
   mkdir $VENDOR_DIRECTORY
 fi
-ANDROID_SDK_DIRECTORY=$VENDOR_DIRECTORY/android-sdt-mac
+ANDROID_SDK_DIRECTORY=$VENDOR_DIRECTORY/android-sdk-macosx
 
 export ANDROID_HOME=$ANDROID_SDK_DIRECTORY
 export PATH=$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools
 
 [ -f ./setup-env ] && . ./setup-env
-
-if [ ! -d $ANDROID_SDK_DIRECTORY/sdk ]; then
+if [ ! -d $ANDROID_SDK_DIRECTORY ]; then
   wget http://dl.google.com/android/$ANDROID_SDK_ARTIFACT
   unzip -d $VENDOR_DIRECTORY $ANDROID_SDK_ARTIFACT
   rm -rf $ANDROID_SDK_ARTIFACT
 fi
 
-android update sdk --no-ui --force --filter platform-tools,extra-android-support,android-17,sysimg-17
+android update sdk --no-ui --force --filter platform-tools,tools
 echo no | android create avd -n emulator -t android-17 --skin WVGA800 --force --abi armeabi-v7a
 
 cd $PROJECT_ROOT
