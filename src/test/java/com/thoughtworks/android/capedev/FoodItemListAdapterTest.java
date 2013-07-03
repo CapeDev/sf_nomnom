9bcd203d4a25e4bad556b735894342c012445da8
Errol Pais <epais@thoughtworks.com>
2013-07-02 21:37:13 -0700

    deleted spikes and unnecessary files

--- src/test/java/com/thoughtworks/android/capedev/FoodItemListAdapterTest.java
+++ src/test/java/com/thoughtworks/android/capedev/FoodItemListAdapterTest.java
@@ -1,25 +1,20 @@
 package com.thoughtworks.android.capedev;
 
-import com.thoughtworks.android.capedev.adapters.FoodItemListAdapter;
-import com.thoughtworks.android.capedev.domain.FoodItem;
+import com.thoughtworks.android.capedev.domain.SearchResult;
 import com.xtremelabs.robolectric.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.util.ArrayList;
-import static junit.framework.Assert.*;
+
+import static junit.framework.Assert.assertEquals;
 
 @RunWith(RobolectricTestRunner.class)
 public class FoodItemListAdapterTest {
 
     @Test
-    public void shouldReturnCount(){
-        ArrayList<FoodItem> foods = new ArrayList<FoodItem>(10);
-        foods.add(new FoodItem("baklava", 1));
-        foods.add(new FoodItem("tiramisu", 2));
-
-        FoodItemListAdapter adapter = new FoodItemListAdapter(Robolectric.application.getApplicationContext(), foods);
-
-        assertEquals(adapter.getCount(), 2);
+    public void shouldBeAnExampleTest(){
+        ArrayList<SearchResult> searchResults = new ArrayList<SearchResult>(10);
+        assertEquals(searchResults.size(), 0);
     }
 }
