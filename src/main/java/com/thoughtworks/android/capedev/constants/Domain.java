52afa86e47273233487b8ee18640a3ef166b4bd9
Errol Pais <epais@thoughtworks.com>
2013-08-06 21:32:35 -0700

    Changed the server to heroku for the demo

--- src/main/java/com/thoughtworks/android/capedev/constants/Domain.java
+++ src/main/java/com/thoughtworks/android/capedev/constants/Domain.java
@@ -6,5 +6,5 @@ public class Domain {
     private static String LOCAL_SERVER = "http://10.0.2.2:3000";
     private static String HEROKU_SERVER = "http://nom-nom-rails.herokuapp.com";
 
-    public static String SERVER_URL= LOCAL_SERVER;
+    public static String SERVER_URL= HEROKU_SERVER;
 }
